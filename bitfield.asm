d29ddf6f2d4497049d2244636d5e2f19f19510c7
ChokshiUtsav <uvc.smart.29@gmail.com>
2016-08-08 18:31:53 +0530

    Added description for all methods of bitfield.asm

--- bitfield.asm
+++ bitfield.asm
@@ -2,13 +2,18 @@
 ;;;;;;;;;; Description ;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;This file contains methods for initializing and setting bitfield of torrent and peer
+;This file contains methods for initializing ,setting and manipulating torrent.bitfield and peer.bitfield
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Procedure Area ;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;initializes bitfield to zeros
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;Desc    : Initializes bitfield to zeros
+;Input   : pointer to torrent data structure and pointer to bitfield
+;Outcome : all bytes of bitfield are set to zero
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 proc torrent._.init_bitfield _torrent, _loc
             
             locals
@@ -38,8 +43,13 @@ proc torrent._.init_bitfield _torrent, _loc
             ret
  endp
 
- ;sets bit in bitfield (_loc : peer.bitfield or torrent.bitfield)
- proc  bitfield._.set_bit _loc, _index
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;Desc    : sets bit in bitfield (_loc : peer.bitfield or torrent.bitfield)
+;Input   : pointer to bitfield , piece-index
+;Outcome : bit that corresponds to piece-index is set to 1
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+proc  bitfield._.set_bit _loc, _index
 
             DEBUGF 2, "INFO : In bitfield._.set_bit\n"
 
@@ -57,21 +67,125 @@ proc torrent._.init_bitfield _torrent, _loc
             ret
  endp
 
- ;gets bit from bitfield
- proc  bitfield._.get_bit _torrent, _loc, _index
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;Desc    : gets bit in bitfield (_loc : peer.bitfield or torrent.bitfield)
+;Input   : pointer to bitfield , piece-index
+;Outcome : eax = 1/0 (bit that corresponds to piece-index)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+proc  bitfield._.get_bit _torrent, _loc, _index
 
             DEBUGF 2, "INFO : In bitfield._.get_bit\n"
 
-            push  esi
+             push  esi
 
-            mov   esi, [_loc]
-            mov   eax, [_index]
-            shr   eax, 3            ;eax=index/8
-            add   esi, eax
-            mov   eax, [_index]
-            and   eax, 7            ;eax=index%8
-            bt    [esi], ax     ;sets bit
+             mov   esi, [_loc]
+             mov   eax, [_index]
+             shr   eax, 3            ;eax=index/8
+             add   esi, eax
+             mov   eax, [_index]
+             and   eax, 7            ;eax=index%8
+             bt    [esi], ax         ;tests bit
+             jc    @f
+             mov   eax, 0
+             jmp   .quit
 
-            pop   esi
-            ret
+       @@:   mov   eax, 1    
+
+      .quit: pop   esi
+             ret
  endp
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;Desc    : Finds first available piece
+;Input   : pointer to torrent data structure and peer data structre
+;Outcome : eax = piece-index
+           eax = -1 (if no piece found)
+;Note    :Availbale piece satisfies following three conditions :
+;1) Peer has the piece available (Check using peer.bitfield)
+;2) Client does not have the piece (Check using torrent.bitfield)
+;3) Piece is not being downloaded by other peer (Check using piece.download_status)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+proc bitfield._.find_first_avl_piece _torrent, _peer
+            
+            locals
+                    size            dd ?
+                    index           dd ?
+            endl
+
+            DEBUGF 2, "INFO : In find_first_avl_piece\n"
+            push    ebx ecx edx esi edi
+
+            mov     eax, [_peer]
+            lea     edi, [eax + peer.bitfield]
+            mov     ebx, [_torrent]
+            lea     esi, [ebx + torrent.bitfield]
+
+            mov     eax, [ebx + torrent.pieces_cnt]
+            shr     eax, 3
+            mov     [size], eax
+            mov     edx, [ebx + torrent.pieces_cnt]
+            and     dx, 7
+
+            mov     ecx, 0
+    .loop:  cmp     ecx, [size]
+            je      .check_lastbyte
+            mov     eax, 0
+            mov     ebx, 0
+            mov     al, byte [esi]
+            mov     bl, byte [edi]
+            not     ax
+            and     bx, ax
+
+    @@:     bsf     ax, bx
+            jz      @f
+
+            mov      [index], ecx
+            shl      [index], 3
+            add      [index], eax
+            stdcall  piece._.get_status, [_torrent], [index]
+            cmp      eax, BT_PIECE_DOWNLOAD_NOT_STARTED
+            je       .quit
+            bsf      ax, bx
+            btr      bx, ax
+            jmp      @b
+
+     @@:    inc     ecx
+            add     esi, 1
+            add     edi, 1
+            jmp     .loop
+
+    .check_lastbyte:
+            cmp      edx, 0
+            je       .error
+            mov      eax, 0
+            mov      ebx, 0
+            mov      al, byte [esi]
+            mov      bl, byte [edi]
+            not      ax
+            and      bx, ax
+    @@:     bsf      ax, bx
+            jz       .error
+            cmp      bx, dx
+            jg       .error
+            imul     ecx, 8
+            mov      [index], ecx
+            add      [index], eax
+            stdcall  piece._.get_status, [_torrent], [index]
+            cmp      eax, BT_PIECE_DOWNLOAD_NOT_STARTED
+            je       .quit
+            bsf      ax, bx
+            btr      bx, ax
+            jmp      @b
+
+    .error: DEBUGF 3, "ERROR : Procedure ended with error\n"
+            mov     eax, -1
+            pop     edi esi edx ecx ebx
+            ret 
+
+    .quit:  DEBUGF 2, "INFO : Procedure ended successfully\n"
+            mov     eax, [index]
+            pop     edi esi edx ecx ebx
+            ret 
+endp
