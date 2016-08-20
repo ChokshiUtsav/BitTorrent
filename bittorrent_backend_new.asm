3748f269ce45ff2f64bddfcbcd67f8d478f0ff71
ChokshiUtsav <uvc.smart.29@gmail.com>
2016-08-21 03:55:31 +0530

    Added new frontend and backend

--- bittorrent_backend_new.asm
+++ bittorrent_backend_new.asm
@@ -11,158 +11,208 @@ use32
         dd      0x01            ; header version
         dd      START           ; entry point
         dd      I_END           ; initialized size
-        dd      E_END   		; required memory
-        dd      E_END		    ; stack pointer
-        dd      0		        ; parameters
+        dd      E_END           ; required memory
+        dd      E_END           ; stack pointer
+        dd      0               ; parameters
         dd      0               ; path
 
 __DEBUG__       = 1
 __DEBUG_LEVEL__ = 1
-BUFFERSIZE		= 1500
+BUFFERSIZE      = 1500
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;Include Area;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-include 'struct.inc'
-include 'proc32.inc'
-include 'macros.inc'
-include 'config.inc'
-include 'network.inc'
-include 'debug-fdo.inc'
-include 'dll.inc'
-include 'libio.inc'
+include 'includes/struct.inc'
+include 'includes/proc32.inc'
+include 'includes/macros.inc'
+include 'includes/config.inc'
+include 'includes/network.inc'
+include 'includes/debug-fdo.inc'
+include 'includes/dll.inc'
+include 'includes/libio.inc'
 include 'torrent.inc'
 include 'torrent_actions.inc'
+include 'torrent_errors.inc'
+include 'bittorrent_backend_actions.asm'
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;Code Area;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 START:
-			;init heap
-		    mcall   68, 11
-		    test    eax, eax
-		    jnz		@f
-		    DEBUGF 3, "ERROR : Problem allocating heap space.\n"
-		    jz      .error
+            ;init heap
+            mcall   68, 11
+            test    eax, eax
+            jnz     @f
+            DEBUGF 3, "ERROR : Problem allocating heap space.\n"
+            jz      .error
 
-			;load libraries
-	@@:	    stdcall dll.Load, @IMPORT
-		    test    eax, eax
-		    jz 		@f
-		    DEBUGF 3, "ERROR : Problem loading libraries.\n"
-		    jmp     .error
+            ;load libraries
+    @@:     stdcall dll.Load, @IMPORT
+            test    eax, eax
+            jz      @f
+            DEBUGF 3, "ERROR : Problem loading libraries.\n"
+            jmp     .error
 
+            ;allocating memory for torrent_info
+    @@:     stdcall  mem.Alloc, (MAX_TORRENTS*sizeof.torrent_info)
+            test     eax, eax
+            jnz      @f
+            DEBUGF 3, "INFO : Not enough memory for torrent_info\n"
+            jmp      .error
 
-		    ;Start listening on port 50000 for incoming requests from frontend
-	@@: 	mcall   40, EVM_STACK
+            ;setting torrent_arr
+    @@:     mov      [torrent_arr], eax
+            mov      [num_torrents], 0
 
-			mcall   socket, AF_INET4, SOCK_STREAM, 0
-    	    cmp     eax, -1
-        	je      .sock_err
-        	mov     [socketnum], eax
+            ;current thread will handle the connection from front-end
+            ;create  thread to handle torrent downloading
 
-        	mcall   bind, [socketnum], sockaddr, sockaddr.length
-        	cmp     eax, -1
-        	je      .bind_err
+            mcall    51, 1, THREAD2_START, [thread_stack]
+            cmp      eax, -1
+            jne      @f
+            DEBUGF 3, "ERROR : Not able to create thread\n"
+            jmp      .error
 
-        	mcall   listen, [socketnum], 10 ; Backlog = 10
-        	cmp     eax, -1
-        	je      .listen_err
+            ;Start listening on port 50000 for incoming requests from frontend
+    @@:     mcall   40, EVM_STACK
+
+            mcall   socket, AF_INET4, SOCK_STREAM, 0
+            cmp     eax, -1
+            je      .sock_err
+            mov     [socketnum], eax
+
+            mcall   bind, [socketnum], sockaddr, sockaddr.length
+            cmp     eax, -1
+            je      .bind_err
+
+            mcall   listen, [socketnum], 10 ; Backlog = 10
+            cmp     eax, -1
+            je      .listen_err
 
     .accept_connection:
-    		DEBUGF 2, "INFO : Listening for incoming connection\n"    	
-    		mcall   accept, [socketnum], sockaddr, sockaddr.length
-        	cmp     eax, -1
-        	je      .accept_err
-        	mov     [socketnum2], eax
+            DEBUGF 2, "INFO : Listening for incoming connection\n"      
+            mcall   accept, [socketnum], sockaddr, sockaddr.length
+            cmp     eax, -1
+            je      .accept_err
+            mov     [socketnum2], eax
 
+            DEBUGF 2, "INFO : Connection accepted\n"
 
-        	DEBUGF 2, "INFO : Connection accepted\n"
-        	mcall   send, [socketnum2], handshake_msg, handshake_msg.length
-        	mcall	close, [socketnum2]
-        	DEBUGF 2, "INFO : Connection closed\n"
-	        jmp		.accept_connection
+    .loop:
+            mcall   recv, [socketnum2], recv_buffer, recv_buffer.length, 0
+            cmp     eax, -1
+            je      .loop
+        
+            mov     byte[recv_buffer+eax], 0            ;eax contains number of bytes copied
 
-  	.loop:
-        	mcall   recv, [socketnum2], buffer, buffer.length, 0
-        	cmp     eax, -1
-        	je      .loop
-		
-        	mov     byte[buffer+eax], 0			;eax contains number of bytes copied
+            ;protocol : <msg-length><msg-type><msg-payload>
+            mov     esi, recv_buffer
+            lodsd
+            mov     [length_of_msg], eax
+            lodsb
 
-        	; protocol : <msg-length><msg-type><msg-payload>
-        	lea		esi, [buffer]
-        	movzx   eax, word[esi]
-        	mov     [length_of_msg], ax
-        	add     esi, 2
+            cmp     al, TORRENT_ACTION_ADD
+            je      .torrent_add
 
-        	cmp     byte[esi], TORRENT_ADD
-        	je		.torrent_add
+            cmp     al, TORRENT_ACTION_START
+            je      .torrent_start
 
-			cmp     byte[esi], TORRENT_START
-        	je		.torrent_start
+            cmp     al, TORRENT_ACTION_PAUSE
+            je      .torrent_pause
 
-			cmp     byte[esi], TORRENT_PAUSE
-        	je		.torrent_pause
+            cmp     al, TORRENT_ACTION_REMOVE
+            je      .torrent_remove
+            
+            cmp     al, TORRENT_ACTION_SHOW
+            je      .torrent_show
 
-			cmp     byte[esi], TORRENT_REMOVE
-        	je		.torrent_remove
-			
-			cmp     byte[esi], TORRENT_SHOW
-        	je		.torrent_show
-
-        	cmp     byte[esi], TORRENT_QUIT
-        	je		.torrent_quit
+            cmp     al, TORRENT_ACTION_SHOW_ALL
+            je      .torrent_show
+            
+            cmp     al, TORRENT_ACTION_QUIT
+            je      .torrent_quit
 
     .torrent_add:
-	        mcall	close, [socketnum2]
-	        jmp		.accept_connection
 
-	.torrent_start:
-			mcall	close, [socketnum2]
-			jmp		.accept_connection
+            ;copy torrent file location
+            mov     edi, torrent_filename
+    .loop1: cmp     byte[esi], '#'
+            je      @f
+            movsb
+            jmp     .loop1
 
-	.torrent_pause:
-			mcall	close, [socketnum2]
-			jmp		.accept_connection
+            ;copy download location
+    @@:     mov     byte[edi], 0x00
+            inc     esi
+            mov     edi, download_location
+    .loop2: cmp     byte[esi], '#'
+            je      @f
+            movsb
+            jmp     .loop2
 
-	.torrent_remove:
-			mcall	close, [socketnum2]
-			jmp		.accept_connection
+    @@:     mov     byte[edi], 0x00
 
-	.torrent_show:
-			mcall	close, [socketnum2]
-			jmp		.accept_connection
+            stdcall backend_actions.torrent_add, torrent_filename, download_location
+            cmp     eax, -1
+            je      @f
+            mcall   send, [socketnum2], torrent_add_suc, torrent_add_suc.length
+            mcall   close, [socketnum2]
+            jmp     .accept_connection
 
-	.torrent_quit:
-			mcall	close, [socketnum2]
-			jmp		.accept_connection
+    @@:     mcall   send, [socketnum2], torrent_add_fail, torrent_add_fail.length
+            mcall   close, [socketnum2]
+            jmp     .accept_connection
 
-	.sock_err:
-			DEBUGF 3, "ERROR : Could not open socket.\n"
-			jmp		.error
+    .torrent_start:
+            mcall   close, [socketnum2]
+            jmp     .accept_connection
 
-	.bind_err:
-			DEBUGF 3, "ERROR : Bind error.\n"
-			jmp		.error
+    .torrent_pause:
+            mcall   close, [socketnum2]
+            jmp     .accept_connection
 
-	.listen_err:
-			DEBUGF 3, "ERROR : Listen error.\n"
-			jmp		.error
+    .torrent_remove:
+            mcall   close, [socketnum2]
+            jmp     .accept_connection
 
-	.accept_err:
-			DEBUGF 3, "ERROR : Accept error.\n"
-			jmp		.error		
+    .torrent_show:
+            mcall   close, [socketnum2]
+            jmp     .accept_connection
 
-	.error: DEBUGF 3, "ERROR : Program ended with error.\n"
+    .torrent_quit:
+            mcall   close, [socketnum2]
+            jmp     .accept_connection
 
+    .sock_err:
+            DEBUGF 3, "ERROR : Could not open socket.\n"
+            jmp     .error
 
-	.exit:  DEBUGF 2, "INFO : Program exited successfully.\n"    
+    .bind_err:
+            DEBUGF 3, "ERROR : Bind error.\n"
+            jmp     .error
 
+    .listen_err:
+            DEBUGF 3, "ERROR : Listen error.\n"
+            jmp     .error
 
+    .accept_err:
+            DEBUGF 3, "ERROR : Accept error.\n"
+            jmp     .error      
+
+    .error: DEBUGF 3, "ERROR : Program ended with error.\n"
+            mcall    -1
+
+    .exit:  DEBUGF 2, "INFO : Program exited successfully.\n"    
+            mcall    -1
+
+THREAD2_START:
+            DEBUGF 2, "INFO : I am at thread 2\n"
+            mcall    -1    
+            
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;Import Area;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -170,22 +220,12 @@ START:
 align 16
 @IMPORT:
 
-library	\
-        libio,   'libio.obj',\
+library \
         torrent, 'torrent.obj'
 
-import 	libio,\
-        libio.init, 'lib_init'  ,   \
-        file.open , 'file_open' ,   \
-        file.read , 'file_read' ,   \
-        file.write , 'file_write' , \
-        file.seek, 'file_seek',     \
-        file.close, 'file_close',   \
-        file.size,	'file_size'
-
 import torrent,\
-	   lib_init, 	 'lib_init',    \
-	   torrent.new,  'new',         \
+       lib_init,     'lib_init',    \
+       torrent.new,  'new',         \
        torrent.start,'start'    
 
 include_debug_strings
@@ -195,30 +235,44 @@ include_debug_strings
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 sockaddr:
-        		dw AF_INET4
-		.port   dw 0x50C3             ; port 50000 = 0xC350 -in network byte order
-		.ip     dd 0
-        		rb 10
-		.length = $ - sockaddr
+                dw AF_INET4
+        .port   dw 23 shl 8             ; port 50000 = 0xC350 -in network byte order
+        .ip     dd 0
+                rb 10
+        .length = $ - sockaddr
 
-handshake_msg	db 'Kolibrios_Bittorrent_Client',0
-.length			=  $ - handshake_msg
+handshake_msg   db 'Kolibrios_Bittorrent_Client',0
+.length         =  $ - handshake_msg
 
-length_of_msg	dw	0
-type_of_msg		db  0
-torrent_id		dd  0
+torrent_add_suc db 'Torrent added successfully', 0
+.length         =  $ - torrent_add_suc
 
-torrent_filename1 db '/usbhd0/1/test.torrent',0
-torrent_filename  rb 512
+torrent_add_fail db 'Torrent added with failure', 0
+.length          =  $ - torrent_add_fail
+
+
+length_of_msg   dd  ?
+type_of_msg     db  0
+torrent_id      dd  0
+
+torrent_arr     dd ?
+num_torrents    dd 0
+thread_stack    dd 0x1000
+thread_id       dd ?
 
 I_END:
 
-socketnum		dd 0
-socketnum2		dd 0
-buffer          rb BUFFERSIZE
-.length 		=  BUFFERSIZE
+socketnum            dd 0
+socketnum2           dd 0
+recv_buffer          rb BUFFERSIZE
+.length               = BUFFERSIZE
+send_buffer          rb BUFFERSIZE
+.length               = BUFFERSIZE
+
+torrent_filename     rb 1024
+download_location    rb 1024
 
 align 4
-rb 0x1000      	; stack
+rb 0x1000       ; stack
 
 E_END:
---
+++ bittorrent_frontend_new.asm
@@ -0,0 +1,328 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;Header Area;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+format binary as ""
+
+;standard header
+use32
+        org     0x0
+        db      'MENUET01'      ; signature
+        dd      0x01            ; header version
+        dd      START           ; entry point
+        dd      I_END           ; initialized size
+        dd      E_END+0x1000    ; required memory
+        dd      E_END+0x1000    ; stack pointer
+        dd      params          ; parameters
+        dd      0               ; path
+
+__DEBUG__       = 1
+__DEBUG_LEVEL__ = 1
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;Include Area;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+include 'includes/struct.inc'
+include 'includes/proc32.inc'
+include 'includes/macros.inc'
+include 'includes/config.inc'
+include 'includes/network.inc'
+include 'includes/debug-fdo.inc'
+include 'includes/dll.inc'
+include 'includes/libio.inc'
+include 'torrent_actions.inc'
+include 'bittorrent_frontend.inc'
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;Code Area;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+START:
+            ;init heap
+            mcall   68, 11
+            test    eax, eax
+            jnz     @f
+            DEBUGF 3, "ERROR : Problem allocating heap space.\n"
+            jz      .error
+
+            ;load libraries
+    @@:     stdcall dll.Load, @IMPORT
+            test    eax, eax
+            jz      @f
+            DEBUGF 3, "ERROR : Problem loading libraries.\n"
+            jmp     .error
+
+            ;initialize console
+    @@:     invoke  con_start, 1
+            invoke  con_init, 80, 25, 80, 25, title
+            invoke  con_write_asciiz, welcome_str
+            invoke  con_write_asciiz, avl_cmd_str
+            invoke  con_write_asciiz, download_cmd_str
+            invoke  con_write_asciiz, show_cmd_str
+            invoke  con_write_asciiz, show_all_cmd_str
+
+    .input_loop:        
+            ;user input
+            invoke  con_write_asciiz, prompt_str
+            invoke  con_gets, params, 1024
+
+            stdcall compare_strs, params, download_cmd_str
+            cmp     eax, -1
+            je      @f
+            stdcall torrent_add
+            jmp     .input_loop
+            
+        @@: stdcall compare_strs, params, show_cmd_str
+            cmp     eax, -1
+            je      @f
+
+        @@: stdcall compare_strs, params, show_all_cmd_str
+            cmp     eax, -1     
+
+            ;close  console
+            invoke  con_get_flags
+            test    eax, 0x200                      ; con window closed?
+            je      .exit
+
+    .error: DEBUGF 3, "ERROR : Program ended with error.\n"
+            mcall    -1
+
+    .exit:  DEBUGF 2, "INFO : Program exited successfully.\n"    
+            mcall    -1
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;Desc    : Compares two null terminated strings
+;Input   : source string pointer, destination string pointer
+;Outcome : if success => eax = 0 (matches)
+;          if error => eax = -1 (does not match)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+proc compare_strs _src, _dest
+            
+            push esi edi
+
+            mov  esi, [_src]
+            mov  edi, [_dest]
+
+    .loop:  mov  al, byte[edi]
+            cmp  byte[esi], al
+            jne  .error
+            cmp  byte[esi], 0x00
+            je   .quit
+            inc  esi
+            inc  edi
+            jmp  .loop
+
+    .error: mov  eax, -1        
+            pop  edi esi
+            ret
+
+    .quit:  mov  eax, 0
+            pop  edi esi
+            ret
+endp
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;Desc    : Copies a null terminated string
+;Input   : source string pointer, destination string pointer
+;Outcome : eax = number of bytes copied
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+proc  copy_strs _src, _dest
+            
+            push ecx esi edi
+
+            mov  esi, [_src]
+            mov  edi, [_dest]
+            mov  ecx, 0
+
+    .loop:  cmp  byte[esi], 0x00
+            je   .quit
+            movsb
+            inc  ecx
+            jmp  .loop
+
+    .quit:  mov  eax, ecx
+            pop  edi esi ecx
+            ret
+endp
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;Desc     : Connects with backend, takes request from send_buffer and stores at recv_buffer
+;Outcome  : if success -> eax = 0   (connected to server)
+;           if error   -> eax = -1  (not able to connect to server)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+proc  connect_backend
+
+            push  ebx ecx edx esi edi
+
+             mcall   40, EVM_STACK
+
+             mov     eax, [localhost_ip]
+             mov     [sockaddr_server.ip],eax
+
+            ;Opening a socket
+             mcall   socket, AF_INET4, SOCK_STREAM, 0
+             cmp     eax, -1
+             jnz     @f
+             DEBUGF 3, "ERROR : Open socket : %d\n",ebx
+             jmp     .error
+
+            ;Connecting with tcp-server
+        @@:  mov     [socketnum], eax
+             mcall   connect, [socketnum], sockaddr_server, sockaddr_server.length
+             cmp     eax, -1
+             jnz     @f
+             DEBUGF 3, "ERROR : Connect %d\n",ebx
+             jmp     .error
+
+             ;sending message
+        @@:  mcall   send, [socketnum], send_buffer, send_msg_len
+             cmp     eax, -1
+             jnz     @f
+             DEBUGF 3, "ERROR: send %d\n",ebx
+             jmp     .error
+
+             ;receving message
+        @@:  mcall   recv, [socketnum], recv_buffer, BUFFERSIZE, 0
+             cmp     eax, -1
+             jnz     .quit
+             DEBUGF 3, "ERROR : recv %d\n",ebx
+             jmp     .error  
+
+    .error: mcall   close, [socketnum]
+            mov  eax, -1        
+            pop  edi esi edx ecx ebx
+            ret
+
+    .quit:  mcall   close, [socketnum]
+            mov  eax, 0
+            pop  edi esi edx ecx ebx
+            ret         
+endp
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;Desc    : Handles torrent download command
+;Outcome : It prints on the console whether torrent is added or not.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+proc torrent_add
+
+            push    ebx ecx edx esi edi
+
+            ;prepares message
+            mov     ecx, 0
+            mov     edi, send_buffer
+            add     edi, 4
+            mov     eax, TORRENT_ACTION_ADD
+            stosb
+
+            ;asks user for torrent file location
+            invoke  con_write_asciiz, torrent_file_str
+            invoke  con_gets, params, 1024
+            stdcall copy_strs, params, edi
+            add     ecx, eax
+            add     edi, eax
+            mov     byte[edi], '#'
+            inc     ecx
+            inc     edi
+
+            ;asks user for torrent download location
+            invoke  con_write_asciiz, download_loc_str
+            invoke  con_gets, params, 1024
+            stdcall copy_strs, params, edi
+            add     ecx, eax
+            add     edi, eax
+            mov     byte[edi], '#'
+            inc     ecx
+            inc     edi
+
+            ;loads message length
+            mov     edi, send_buffer
+            mov     eax, ecx
+            stosd
+
+            ;connecting to backend
+            invoke  con_write_asciiz, connect_backend_str
+            add     ecx, 5
+            mov     [send_msg_len], ecx
+            stdcall connect_backend
+            cmp     eax, -1
+            jne     @f
+            invoke  con_write_asciiz, problem_backend_str
+            jmp     .quit
+
+    @@:     invoke  con_write_asciiz, recv_buffer   
+
+    .quit:  pop  edi esi edx ecx ebx
+            ret
+endp
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;Import Area;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+align 16
+@IMPORT:
+
+library console, 'console.obj'
+
+import  console,        \
+        con_start,      'START',        \
+        con_init,       'con_init',     \
+        con_write_asciiz,       'con_write_asciiz',     \
+        con_exit,       'con_exit',     \
+        con_gets,       'con_gets',\
+        con_cls,        'con_cls',\
+        con_printf,     'con_printf',\
+        con_getch2,     'con_getch2',\
+        con_set_cursor_pos, 'con_set_cursor_pos',\
+        con_get_flags,  'con_get_flags'
+
+include_debug_strings
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;Data Area;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;console strings
+title               db      'Bittorrent Client v1.0',0
+welcome_str         db      'Welcome to Bittorrent Client v1.0 ... ',10,0
+avl_cmd_str         db      'Following commands are available :',10,0
+download_cmd_str    db      'download_torrent',10,0
+show_cmd_str        db      'show_torrent',10,0
+show_all_cmd_str    db      'show_all_torrent',10,0
+torrent_file_str    db      '>>>>Enter torrent file location :', 10,0
+download_loc_str    db      '>>>>Enter download location :', 10,0
+connect_backend_str db      '>>>>Connecting to backend...',10,0
+problem_backend_str db      '>>>>Problem connecting with backend...',10,0
+prompt_str          db       10,'>> ',0
+
+;Data for connecting with backend
+sockaddr_server:  
+                        dw AF_INET4
+    .port               dw 23 shl 8             ; port 50000 = 0xC350 -in network byte order     
+    .ip                 dd 0
+                        rb 10
+    .length             =  $ - sockaddr_server
+
+localhost_ip:
+                        db 127
+                        db 0
+                        db 0
+                        db 1
+
+socketnum               dd 0
+
+send_msg_len            dd ?
+
+I_END:
+
+send_buffer         rb      BUFFERSIZE
+recv_buffer         rb      BUFFERSIZE
+params              rb      1024
+
+E_END:
