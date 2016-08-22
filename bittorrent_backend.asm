5e47d553dd7e789ab01728e6f2c97107b520434c
ChokshiUtsav <uvc.smart.29@gmail.com>
2016-08-22 17:00:39 +0530

    Updated backend code

--- bittorrent_backend.asm
+++ bittorrent_backend.asm
@@ -32,7 +32,9 @@ include 'includes/debug-fdo.inc'
 include 'includes/dll.inc'
 include 'includes/libio.inc'
 include 'torrent.inc'
-
+include 'torrent_actions.inc'
+include 'torrent_errors.inc'
+include 'bittorrent_backend_actions.asm'
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;Code Area;;;;;;;;;;;;;;;;;;;
@@ -53,30 +55,136 @@ START:
             DEBUGF 3, "ERROR : Problem loading libraries.\n"
             jmp     .error
 
-            ;Add new torrent
-    @@:     DEBUGF 2, "INFO : Calling torrent.new\n"
-            invoke  torrent.new, BT_NEW_FILE, torrent_filename1, download_location
-            cmp     eax, -1
-            jne     @f
-            DEBUGF 3, "ERROR : Problem with torrent.new\n"
-            jmp     .error 
+            ;allocating memory for torrent_info
+    @@:     stdcall  mem.Alloc, (MAX_TORRENTS*sizeof.torrent_info)
+            test     eax, eax
+            jnz      @f
+            DEBUGF 3, "INFO : Not enough memory for torrent_info\n"
+            jmp      .error
 
-    @@:     DEBUGF 2, "INFO : Successfully returned from torrent.new\n"
-            DEBUGF 2, "INFO : Calling torrent.start\n"
-            invoke  torrent.start, eax
-            cmp     eax, -1
-            jnz     @f
-            DEBUGF 3, "ERROR : Problem with torrent.start\n"
-            jmp     .error            
+            ;setting torrent_arr
+    @@:     mov      [torrent_arr], eax
+            mov      [num_torrents], 0
 
-    @@:     DEBUGF 2, "INFO : Successfully returned from torrent.start\n"
-            jmp .exit
+            ;current thread will handle the connection from front-end
+            ;create  thread to handle torrent downloading
+
+    @@:     mcall    51, 1, THREAD2_START, [thread_stack]
+            cmp      eax, -1
+            jne      @f
+            DEBUGF 3, "ERROR : Not able to create thread\n"
+            jmp      .error
+
+            ;Start listening on port 50000 for incoming requests from frontend
+    @@:     mcall   40, EVM_STACK
+
+            mcall   socket, AF_INET4, SOCK_STREAM, 0
+            cmp     eax, -1
+            je      .sock_err
+            mov     [socketnum], eax
+
+            mcall   bind, [socketnum], sockaddr, sockaddr.length
+            cmp     eax, -1
+            je      .bind_err
+
+            mcall   listen, [socketnum], 10 ; Backlog = 10
+            cmp     eax, -1
+            je      .listen_err
+
+    .accept_connection:
+            DEBUGF 2, "INFO : Listening for incoming connection\n"      
+            mcall   accept, [socketnum], sockaddr, sockaddr.length
+            cmp     eax, -1
+            je      .accept_err
+            mov     [socketnum2], eax
+            DEBUGF 2, "INFO : Connection accepted\n"
+
+    .loop:
+            mcall   recv, [socketnum2], recv_buffer, recv_buffer.length, 0
+            cmp     eax, -1
+            je      .loop
+        
+            mov     byte[recv_buffer+eax], 0            ;eax contains number of bytes copied
+
+            ;protocol : <msg-length><msg-type><msg-payload>
+            mov     esi, recv_buffer
+            lodsd
+            mov     [length_of_msg], eax
+            lodsb
+            mov     [type_of_msg], al   
+
+            cmp     al, TORRENT_ACTION_ADD
+            je      .torrent_add
+
+            cmp     al, TORRENT_ACTION_START
+            je      .torrent_start
+
+            cmp     al, TORRENT_ACTION_PAUSE
+            je      .torrent_pause
+
+            cmp     al, TORRENT_ACTION_REMOVE
+            je      .torrent_remove
+            
+            cmp     al, TORRENT_ACTION_SHOW
+            je      .torrent_show
+
+            cmp     al, TORRENT_ACTION_SHOW_ALL
+            je      .torrent_show
+            
+            cmp     al, TORRENT_ACTION_QUIT
+            je      .torrent_quit
+
+    .torrent_add:
+            stdcall backend_actions.torrent_add, esi, send_buffer
+            mcall   send, [socketnum2], send_buffer, eax
+            mcall   close, [socketnum2]
+            jmp     .accept_connection
+
+    .torrent_start:
+            mcall   close, [socketnum2]
+            jmp     .accept_connection
+
+    .torrent_pause:
+            mcall   close, [socketnum2]
+            jmp     .accept_connection
+
+    .torrent_remove:
+            mcall   close, [socketnum2]
+            jmp     .accept_connection
+
+    .torrent_show:
+            mcall   close, [socketnum2]
+            jmp     .accept_connection
+
+    .torrent_quit:
+            mcall   close, [socketnum2]
+            jmp     .accept_connection
+
+    .sock_err:
+            DEBUGF 3, "ERROR : Could not open socket.\n"
+            jmp     .error
+
+    .bind_err:
+            DEBUGF 3, "ERROR : Bind error.\n"
+            jmp     .error
+
+    .listen_err:
+            DEBUGF 3, "ERROR : Listen error.\n"
+            jmp     .error
+
+    .accept_err:
+            DEBUGF 3, "ERROR : Accept error.\n"
+            jmp     .error  
 
     .error: DEBUGF 3, "ERROR : Program ended with error.\n"
             mcall   -1
 
     .exit:  DEBUGF 2, "INFO : Program exited successfully.\n"    
             mcall   -1
+
+THREAD2_START:
+            DEBUGF 2, "INFO : I am at thread 2\n"
+            mcall    -1    
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;Import Area;;;;;;;;;;;;;;;;;
@@ -101,7 +209,8 @@ import  libio,\
 import torrent,\
        lib_init,     'lib_init',    \
        torrent.new,  'new',         \
-       torrent.start,'start'    
+       torrent.start,'start',       \
+       torrent.resume,'resume'   
 
 include_debug_strings
 
@@ -109,8 +218,41 @@ include_debug_strings
 ;;;;;;;;;;;;;Data Area;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-torrent_filename1 db '/usbhd0/1/test.torrent',0
-download_location db '/tmp0/1',0
+;Data for all torrents
+torrent_arr          dd ?
+num_torrents         dd 0
+torrent_filename1    db '/usbhd0/1/test.torrent',0
+download_location1   db '/tmp0/1',0
+torrent1             dd ?
+torrent_add_fail     db 'Torrent added with failure', 0
+.length              =  $ - torrent_add_fail
+torrent_add_suc      db 'Torrent added successfully', 0
+.length              =  $ - torrent_add_suc
+
+
+;Data for connection
+socketnum            dd ?
+socketnum2           dd ?
+recv_buffer          rb BUFFERSIZE
+.length               = BUFFERSIZE
+send_buffer          rb BUFFERSIZE
+.length               = BUFFERSIZE
+length_of_msg        dd ?
+type_of_msg          db ?
+
+sockaddr:
+                     dw AF_INET4
+        .port        dw 23 shl 8             ; port 50000 = 0xC350 -in network byte order
+        .ip          dd 0
+                     rb 10
+        .length      = $ - sockaddr
+
+
+
+;Data for thread
+thread_stack    dd 0x1000
+thread_id       dd ?
+
 
 I_END:
     rb 0x1000                   ; stack
--- bittorrent_backend_actions.asm
+++ bittorrent_backend_actions.asm
@@ -9,7 +9,34 @@
 ;;;;;;; Procedure Area;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;Desc    : Copies a null terminated string
+;Input   : source string pointer, destination string pointer
+;Outcome : eax = number of bytes copied
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+proc  copy_strs _dest, _src
+            
+            push ecx esi edi
+
+            mov  esi, [_src]
+            mov  edi, [_dest]
+            mov  ecx, 0
+
+    .loop:  cmp  byte[esi], 0x00
+            je   .quit
+            movsb
+            inc  ecx
+            jmp  .loop
+
+    .quit:  mov  byte[edi], 0x00
+            mov  eax, ecx
+            pop  edi esi ecx
+            ret
+endp
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;Desc  : adds new torrent
 ;Input : location of .torrent file, download location 
 ;Outcome : if success -> eax = torrent_id
@@ -17,8 +44,7 @@
 ;                        ebx = errorcode
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-proc backend_actions.torrent_add _torrentfile, _downloadloc
-    
+proc backend_actions.torrent_add _msg, _sendbuffer
             DEBUGF 2, "INFO : In backend_actions.torrent_add\n"
 
             push    ecx edx esi edi
@@ -30,14 +56,34 @@ proc backend_actions.torrent_add _torrentfile, _downloadloc
             mov     ebx, BT_ERROR_MAX_TORRENT_EXCEED
             jmp     .error
 
+    @@:     mov     esi, [_msg]
+            ;copy torrent file location
+            mov     edi, torrent_filename
+    .loop1: cmp     byte[esi], '#'
+            je      @f
+            movsb
+            jmp     .loop1
+
+            ;copy download location
+    @@:     dec     edi
+            mov     byte[edi], 0x00
+            inc     esi
+            mov     edi, download_location
+    .loop2: cmp     byte[esi], '#'
+            je      @f
+            movsb
+            jmp     .loop2
+
+     @@:    dec     edi
+            mov     byte[edi], 0x00
 
             ;adding new torrent
-    @@:     invoke   torrent.new, BT_NEW_FILE, [_torrentfile], [_downloadloc]
+    @@:     invoke   torrent.new, BT_NEW_FILE, torrent_filename, download_location
             cmp      eax, -1
             jne      @f
             DEBUGF 3, "ERROR : Problem with torrent.new\n"
             jmp      .error
-
+            
             ;adding torrent to torrent_arr
     @@:     mov      edi, [num_torrents]
             imul     edi, sizeof.torrent_info
@@ -48,14 +94,30 @@ proc backend_actions.torrent_add _torrentfile, _downloadloc
             mcall    26, 9
             lodsd                       ;setting torrent_id
             inc      [num_torrents]
+            stdcall  copy_strs, [_sendbuffer], Torrent_Add_Suc_Str
+            jmp      .quit
 
-    .error: DEBUGF 3, "ERROR : Procedure ended with error\n"
-            mov     eax, -1
-            pop     edi esi edx ecx
-            ret
+    .error: cmp      ebx, BT_ERROR_NOT_ENOUGH_MEMORY
+            jne      @f
+            stdcall  copy_strs, [_sendbuffer], Not_Enough_Memory_Str
+            jmp      .quit
+    
+    @@:     cmp      ebx, BT_ERROR_INSUFF_HD_SPACE
+            jne      @f
+            stdcall  copy_strs, [_sendbuffer], Insuff_Hd_Space_Str
+            jmp      .quit
+
+    @@:     cmp      ebx, BT_ERROR_INVALID_TORRENT_FILE
+            jne      @f
+            stdcall  copy_strs, [_sendbuffer], Invalid_Torrent_File_Str
+            jmp      .quit
+
+    @@:     cmp      ebx, BT_ERROR_MAX_TORRENT_EXCEED 
+            jne      @f
+            stdcall  copy_strs, [_sendbuffer], Max_Torrent_Exceed_Str
 
     .quit:  DEBUGF 2, "INFO : Procedure ended successfully\n"
-            pop     edi esi edx ecx
+            pop      edi esi edx ecx
             ret
 endp
 
@@ -102,3 +164,11 @@ endp
 
 proc prepare_torrent_details _torrentid
 endp
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;; Data Area ;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+torrent_filename     rb 512
+download_location    rb 512
